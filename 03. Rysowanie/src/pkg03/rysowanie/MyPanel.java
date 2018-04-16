b6fcea9b17f1ee78419bf83c71293c902e5f15ac
marwis95 <marwis95@o2.pl>
2018-04-16 20:16:42 +0200

    Drawing 2

--- 03. Rysowanie/src/pkg03/rysowanie/MyPanel.java
+++ 03. Rysowanie/src/pkg03/rysowanie/MyPanel.java
@@ -6,6 +6,8 @@
 package pkg03.rysowanie;
 
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 import javax.swing.JPanel;
 
 /**
@@ -22,9 +24,17 @@ public class MyPanel extends JPanel{
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D)g;
         
-        g2d.drawRect(10, 10, 380, 380);
         
-        g2d.drawOval(10, 10, 380, 380);
+        //Sposób 1
+        //g2d.drawRect(10, 10, 380, 380);
+        //g2d.drawOval(10, 10, 380, 380);
+        
+        //Sposób 2
+        Rectangle2D rectangle = new Rectangle2D.Double(10, 10, 380, 380);
+        Ellipse2D ellipse = new Ellipse2D.Double(10, 10, 380, 380);
+        
+        g2d.draw(rectangle);
+        g2d.draw(ellipse);
         
     }
     
